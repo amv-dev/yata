88e01e817807026cc230b895553db6ced3dd75ed
amv-dev <amv-dev@protonmail.com>
2020-11-05 15:43:22 +0500

    Chande Kroll Stop fixes

--- src/indicators/chande_kroll_stop.rs
+++ src/indicators/chande_kroll_stop.rs
@@ -4,11 +4,30 @@ use serde::{Deserialize, Serialize};
 
 use crate::core::{Action, Error, Method, PeriodType, Source, ValueType, OHLC};
 use crate::core::{IndicatorConfig, IndicatorInitializer, IndicatorInstance, IndicatorResult};
-use crate::helpers::{method, RegularMethod, RegularMethods};
-use crate::methods::{Highest, Lowest};
+use crate::helpers::{method, signi, RegularMethod, RegularMethods};
+use crate::methods::{CrossAbove, Highest, Lowest};
 
 // ChandeKrollStop p=10, x=1.0, q=9
-/// [Chande Kroll Stop](https://patternswizard.com/chande-kroll-stop-indicator/)
+/// [Chande Kroll Stop](https://tradingview.com/support/solutions/43000589105-chande-kroll-stop/)
+///
+/// # 3 values
+///
+/// * `stop long`
+/// * `source` value
+/// * `stop short`
+///
+/// # 2 signals
+///
+/// * signal 1 is calculated according to relative position of the `source` value between `stop short` and `stop long` values.
+///
+/// When `source` value goes above `stop short`, then returns full buy signal.
+///
+/// When `source` value goes below `stop long`, then returns full sell signal.
+/// * signal 2 appears only when `stop long` crosses `stop short` upwards.
+///
+/// When cumulative move of `stop short` and `stop long` is upwards, then returns full buy.
+///
+/// When cumulative move of `stop short` and `stop long` is downwards, then returns full sell.
 #[derive(Debug, Clone, Copy)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct ChandeKrollStop {
@@ -16,7 +35,7 @@ pub struct ChandeKrollStop {
 	pub p: PeriodType,
 	/// ATR method. Default is [`SMA`](crate::methods::SMA).
 	pub method: RegularMethods,
-	/// ATR multiplier. Default is 1.0.
+	/// ATR multiplier. Can be negative. Default is 1.0.
 	pub x: ValueType,
 	/// multiplied highest/lowest period length. Default is 9.
 	pub q: PeriodType,
@@ -59,7 +78,7 @@ impl IndicatorConfig for ChandeKrollStop {
 	}
 
 	fn size(&self) -> (u8, u8) {
-		(3, 1)
+		(3, 2)
 	}
 }
 
@@ -74,6 +93,8 @@ impl<T: OHLC> IndicatorInitializer<T> for ChandeKrollStop {
 			return Err(Error::WrongConfig);
 		}
 
+		let tr = candle.high() - candle.low();
+
 		let cfg = self;
 		Ok(Self::Instance {
 			ma: method(cfg.method, cfg.p, candle.tr(&candle))?,
@@ -81,10 +102,16 @@ impl<T: OHLC> IndicatorInitializer<T> for ChandeKrollStop {
 			highest1: Highest::new(cfg.p, candle.high())?,
 			lowest1: Lowest::new(cfg.p, candle.low())?,
 
-			highest2: Highest::new(cfg.q, candle.high())?,
-			lowest2: Lowest::new(cfg.q, candle.low())?,
+			highest2: Highest::new(cfg.q, candle.high() - cfg.x * tr)?,
+			lowest2: Lowest::new(cfg.q, cfg.x.mul_add(tr, candle.low()))?,
 
 			prev_candle: candle,
+			prev_stop_short: candle.high() - cfg.x * tr,
+			prev_stop_long: cfg.x.mul_add(tr, candle.low()),
+			cross_above: CrossAbove::new(
+				(),
+				(cfg.x.mul_add(tr, candle.low()), candle.high() - cfg.x * tr),
+			)?,
 			cfg,
 		})
 	}
@@ -113,6 +140,9 @@ pub struct ChandeKrollStopInstance<T: OHLC> {
 	highest2: Highest,
 	lowest2: Lowest,
 	prev_candle: T,
+	prev_stop_short: ValueType,
+	prev_stop_long: ValueType,
+	cross_above: CrossAbove,
 }
 
 impl<T: OHLC> IndicatorInstance<T> for ChandeKrollStopInstance<T> {
@@ -129,11 +159,8 @@ impl<T: OHLC> IndicatorInstance<T> for ChandeKrollStopInstance<T> {
 
 		let atr = self.ma.next(tr);
 
-		let highest = &mut self.highest1;
-		let lowest = &mut self.lowest1;
-
-		let phs = highest.next(candle.high()) - atr * self.cfg.x;
-		let pls = atr.mul_add(self.cfg.x, lowest.next(candle.low()));
+		let phs = self.highest1.next(candle.high()) - atr * self.cfg.x;
+		let pls = atr.mul_add(self.cfg.x, self.lowest1.next(candle.low()));
 
 		let stop_short = self.highest2.next(phs);
 		let stop_long = self.lowest2.next(pls);
@@ -142,8 +169,18 @@ impl<T: OHLC> IndicatorInstance<T> for ChandeKrollStopInstance<T> {
 
 		let mid = (stop_short + stop_long) * 0.5;
 		let size = mid - stop_long;
-		let value = (src - mid) / size;
 
-		IndicatorResult::new(&[stop_long, src, stop_short], &[Action::from(value)])
+		let value = if size == 0.0 { 0.0 } else { (src - mid) / size };
+
+		#[allow(unused_parens)]
+		let s2_diff = (stop_short - self.prev_stop_short) + (stop_long - self.prev_stop_long);
+		let is_s2 = (stop_short < stop_long) as i8; // s2 should appear only when `STOP LONG` is above `STOP SHORT`
+		let cross: i8 = self.cross_above.next((stop_long, stop_short)).into(); // also s2 should appear only when `STOP LONG` actually crossing `STOP SHORT` upwards
+		let s2 = cross * is_s2 * signi(s2_diff);
+
+		IndicatorResult::new(
+			&[stop_long, src, stop_short],
+			&[Action::from(value), Action::from(s2)],
+		)
 	}
 }
