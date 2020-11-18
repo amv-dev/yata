012f616583191c749839403c94203a5a6af09417
amv-dev <amv-dev@protonmail.com>
2020-11-19 03:06:56 +0500

    fisher transform refactoring

--- src/indicators/fisher_transform.rs
+++ src/indicators/fisher_transform.rs
@@ -16,7 +16,6 @@ pub struct FisherTransform {
 	pub period1: PeriodType,
 	pub period2: PeriodType,
 	pub zone: ValueType,
-	pub delta: PeriodType,
 	pub method: RegularMethods,
 	pub source: Source,
 }
@@ -25,7 +24,7 @@ impl IndicatorConfig for FisherTransform {
 	const NAME: &'static str = "FisherTransform";
 
 	fn validate(&self) -> bool {
-		self.period1 >= 3 && self.delta >= 1 && self.period2 >= 1
+		self.period1 > 1 && self.period2 > 1 && self.zone > 0.
 	}
 
 	fn set(&mut self, name: &str, value: String) -> Option<Error> {
@@ -41,10 +40,6 @@ impl IndicatorConfig for FisherTransform {
 			"zone" => match value.parse() {
 				Err(_) => return Some(Error::ParameterParse(name.to_string(), value.to_string())),
 				Ok(value) => self.zone = value,
-			},
-			"delta" => match value.parse() {
-				Err(_) => return Some(Error::ParameterParse(name.to_string(), value.to_string())),
-				Ok(value) => self.delta = value,
 			},
 			"method" => match value.parse() {
 				Err(_) => return Some(Error::ParameterParse(name.to_string(), value.to_string())),
@@ -86,9 +81,10 @@ impl<T: OHLC> IndicatorInitializer<T> for FisherTransform {
 			ma1: method(cfg.method, cfg.period2, 0.)?,
 			highest: Highest::new(cfg.period1, src)?,
 			lowest: Lowest::new(cfg.period1, src)?,
-			cross_over: Cross::default(),
-			extreme: 0,
+			cross: Cross::default(),
+			cross_ma: Cross::default(),
 			prev_value: 0.,
+			last_reverse: 0,
 			cfg,
 		})
 	}
@@ -100,7 +96,6 @@ impl Default for FisherTransform {
 			period1: 9,
 			period2: 1,
 			zone: 1.5,
-			delta: 1,
 			method: RegularMethods::SMA,
 			source: Source::TP,
 		}
@@ -114,9 +109,10 @@ pub struct FisherTransformInstance {
 	ma1: RegularMethod,
 	highest: Highest,
 	lowest: Lowest,
-	cross_over: Cross,
-	extreme: i8,
+	cross: Cross,
+	cross_ma: Cross,
 	prev_value: ValueType,
+	last_reverse: i8,
 }
 
 const BOUND: ValueType = 0.999;
@@ -136,72 +132,52 @@ impl<T: OHLC> IndicatorInstance<T> for FisherTransformInstance {
 	fn next(&mut self, candle: T) -> IndicatorResult {
 		let src = candle.source(self.cfg.source);
 
-		// converting original value to between -1.0 and 1.0 over period1
-		let h = self.highest.next(src);
-		let l = self.lowest.next(src);
+		// first we need to find MAX and MIN values for last `period1` prices
+		let highest = self.highest.next(src);
+		let lowest = self.lowest.next(src);
+
 		// we need to check division by zero, so we can really just check if `h` is equal to `l` without using any kind of round error checks
-		#[allow(clippy::float_cmp)]
-		let is_different = (h != l) as i8 as ValueType;
-		let v1 = is_different * (src * 2. - (h + l)) / (h - l + 1. - is_different);
+		let fisher_transform = if highest.to_bits() == lowest.to_bits() {
+			0.
+		} else {
+			// converting `SRC` into a value in range [-1; 1]
+			let x = bound_value((src - lowest) / (highest - lowest) * 2. - 1.);
+			// calculating fisher transform value
+			x.atanh()
+		};
 
-		// bound value
-		let bound_val = bound_value(v1);
+		let cumulative = self.prev_value.mul_add(0.5, fisher_transform);
 
-		// calculating fisher transform value
-		let fisher_transform: ValueType = bound_val.atanh(); //((1.0 + v2)/(1.0-v2)).ln();
+		// We’ll take trade signals based on the following rules:
+		// Long trades
 
-		// if fisher_transform > self.cfg.zone {
-		// 	self.extreme = -1;
-		// } else if fisher_transform < self.cfg.zone {
-		// 	self.extreme = 1;
-		// }
-		self.extreme =
-			(fisher_transform < self.cfg.zone) as i8 - (fisher_transform > self.cfg.zone) as i8;
+		// 	Fisher Transform must be negative (i.e., the more negative the indicator is, the more “stretched” or excessively bearish price is)
+		// 	Taken after a reversal of the Fisher Transform from negatively sloped to positively sloped (i.e., rate of change from negative to positive)
 
-		let s1;
-		{
-			// We’ll take trade signals based on the following rules:
-			// Long trades
+		// Short trades
 
-			// 	Fisher Transform must be negative (i.e., the more negative the indicator is, the more “stretched” or excessively bearish price is)
-			// 	Taken after a reversal of the Fisher Transform from negatively sloped to positively sloped (i.e., rate of change from negative to positive)
+		// 	Fisher Transform must be positive (i.e., price perceived to be excessively bullish)
+		// 	Taken after a reversal in the direction of the Fisher Transform
+		let reverse = self.cross.next((cumulative, self.prev_value)).analog();
 
-			// Short trades
+		let s1 = cumulative / self.cfg.zone
+			* ((cumulative < 0.0 && reverse > 0) || (cumulative > 0.0 && reverse < 0)) as i8
+				as ValueType;
 
-			// 	Fisher Transform must be positive (i.e., price perceived to be excessively bullish)
-			// 	Taken after a reversal in the direction of the Fisher Transform
-			// s1 = if self.extreme == 1 && fisher_transform - self.prev_value < 0. {
-			// 	-1
-			// } else if self.extreme == -1 && fisher_transform - self.prev_value > 0. {
-			// 	1
-			// } else {
-			// 	0
-			// };
-			s1 = (self.extreme == -1 && fisher_transform - self.prev_value > 0.) as i8
-				- (self.extreme == 1 && fisher_transform - self.prev_value < 0.) as i8;
-		}
+		// The Fisher Transform frequently has a signal line attached to it. This is a moving average of the Fisher Transform value,
+		// so it moves slightly slower than the Fisher Transform line. When the Fisher Transform crosses the trigger line it is used
+		// by some traders as a trade signal. For example, when the Fisher Transform drops below the signal line after hitting an
+		// extreme high, that could be used as a signal to sell a current long position.
+		let signal_line = self.ma1.next(cumulative);
+		let crossed_ma = self.cross_ma.next((cumulative, signal_line)).analog();
 
-		self.prev_value = fisher_transform;
+		let is_reversed = (reverse != 0) as i8;
+		self.last_reverse = (1 - is_reversed) * self.last_reverse + is_reversed * reverse;
 
-		let s2;
-		let fisher_transform_ma: ValueType;
-		{
-			// The Fisher Transform frequently has a signal line attached to it. This is a moving average of the Fisher Transform value,
-			// so it moves slightly slower than the Fisher Transform line. When the Fisher Transform crosses the trigger line it is used
-			// by some traders as a trade signal. For example, when the Fisher Transform drops below the signal line after hitting an
-			// extreme high, that could be used as a signal to sell a current long position.
-			fisher_transform_ma = self.ma1.next(fisher_transform);
-			let cross = self
-				.cross_over
-				.next((fisher_transform, fisher_transform_ma))
-				.analog();
-			// s2 = if cross * self.extreme == 1 { cross } else { 0 };
-			s2 = ((cross * self.extreme) == 1) as i8 * cross;
-		}
+		let s2 = signal_line / self.cfg.zone
+			* ((signal_line < 0.0 && self.last_reverse > 0 && crossed_ma > 0)
+				|| (signal_line > 0.0 && self.last_reverse < 0 && crossed_ma < 0)) as i8 as ValueType;
 
-		IndicatorResult::new(
-			&[fisher_transform, fisher_transform_ma],
-			&[Action::from(s1), Action::from(s2)],
-		)
+		IndicatorResult::new(&[fisher_transform, signal_line], &[s1.into(), s2.into()])
 	}
 }
