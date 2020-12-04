8ae28fef8274c458706e690cd24018e04615d947
amv-dev <amv-dev@protonmail.com>
2020-12-04 17:21:54 +0500

    added MACD docs

--- src/indicators/macd.rs
+++ src/indicators/macd.rs
@@ -8,16 +8,60 @@ use crate::core::{IndicatorConfig, IndicatorInitializer, IndicatorInstance, Indi
 use crate::helpers::{method, RegularMethod, RegularMethods};
 use crate::methods::Cross;
 
-// https://en.wikipedia.org/wiki/MACD
+/// Moving average convergence/divergence (MACD)
+///
+/// ## Links
+///
+/// * <https://en.wikipedia.org/wiki/MACD>
+///
+/// # 2 values
+///
+/// * `MACD` value
+///
+/// Range in \(`-inf`; `+inf`\).
+///
+/// * `Signal line` value
+///
+/// Range in \(`-inf`; `+inf`\).
+///
+/// # 2 signal
+///
+/// * When `MACD` crosses `Signal line` upwards, returns full buy signal.
+/// When `MACD` crosses `Signal line` downwards, returns full sell signal.
+/// Otherwise returns no signal.
+///
+/// * When `MACD` crosses zero line upwards, returns full buy signal.
+/// When `MACD` crosses zero line downwards, returns full sell signal.
+/// Otherwise returns no signal.
+///
 #[derive(Debug, Clone, Copy)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct MACD {
+	/// Fast MA period. Default is `12`.
+	///
+	/// Range in \[`2`; `period2`\)
 	pub period1: PeriodType,
-	pub period2: PeriodType,
-	pub period3: PeriodType,
+
+	/// Fast MA type. Default is [`EMA`](crate::methods::EMA).
 	pub method1: RegularMethods,
+
+	/// Slow MA period. Default is `26`.
+	///
+	/// Range in \(`period1`; [`PeriodType::MAX`](crate::core::PeriodType)\)
+	pub period2: PeriodType,
+
+	/// Slow MA type. Default is [`EMA`](crate::methods::EMA).
 	pub method2: RegularMethods,
+
+	/// Signal line MA period. Default is `9`.
+	///
+	/// Range in \[`2`; [`PeriodType::MAX`](crate::core::PeriodType)\)
+	pub period3: PeriodType,
+
+	/// Signal line MA type. Default is [`EMA`](crate::methods::EMA).
 	pub method3: RegularMethods,
+
+	/// Source value type. Default is [`Close`](crate::core::Source::Close)
 	pub source: Source,
 }
 
@@ -25,7 +69,7 @@ impl IndicatorConfig for MACD {
 	const NAME: &'static str = "MACD";
 
 	fn validate(&self) -> bool {
-		self.period1 < self.period2
+		self.period1 < self.period2 && self.period1 > 1 && self.period3 > 1
 	}
 
 	fn set(&mut self, name: &str, value: String) -> Option<Error> {
@@ -67,7 +111,7 @@ impl IndicatorConfig for MACD {
 	}
 
 	fn size(&self) -> (u8, u8) {
-		(2, 1)
+		(2, 2)
 	}
 }
 
@@ -85,7 +129,8 @@ impl<T: OHLC> IndicatorInitializer<T> for MACD {
 				ma1: method(cfg.method1, cfg.period1, src)?,
 				ma2: method(cfg.method2, cfg.period2, src)?,
 				ma3: method(cfg.method3, cfg.period3, src)?,
-				cross: Cross::new((), (0.0, 0.0))?,
+				cross1: Cross::new((), (0.0, 0.0))?,
+				cross2: Cross::new((), (0.0, 0.0))?,
 				cfg,
 			})
 		} else {
@@ -115,7 +160,8 @@ pub struct MACDInstance {
 	ma1: RegularMethod,
 	ma2: RegularMethod,
 	ma3: RegularMethod,
-	cross: Cross,
+	cross1: Cross,
+	cross2: Cross,
 }
 
 /// Just an alias for MACD
@@ -144,8 +190,9 @@ impl<T: OHLC> IndicatorInstance<T> for MACDInstance {
 		let macd = ema1 - ema2;
 		let sigline = self.ma3.next(macd);
 
-		let signal = self.cross.next((macd, sigline));
+		let signal1 = self.cross1.next((macd, sigline));
+		let signal2 = self.cross2.next((macd, 0.0));
 
-		IndicatorResult::new(&[macd, sigline], &[signal])
+		IndicatorResult::new(&[macd, sigline], &[signal1, signal2])
 	}
 }
