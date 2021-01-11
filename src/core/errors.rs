90b5c1ca3e06d2d4cdef34eff4f8c6fc37639ad1
Oliver Bristow <evilumbrella+github@gmail.com>
2021-01-11 13:59:47 +0000

    Add traits to core::Error for anyhow support

--- src/core/errors.rs
+++ src/core/errors.rs
@@ -19,3 +19,22 @@ pub enum Error {
 	/// Any other error
 	Other(String),
 }
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::SourceParse(value) => write!(f, "unable to parse value as Source: {:?}", value),
+            Self::ParameterParse(name, value) => write!(f, "unable to parse into {}: {:?}", name, value),
+            Self::WrongMethodParameters => write!(f, "wrong method parameters"),
+            Self::WrongConfig => write!(f, "wrong config"),
+            Self::InvalidCandles => write!(f, "invalid candles"),
+            Self::Other(reason) => write!(f, "{}", reason),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
