a11ed03cf1851d1eceedc3771b4521452658e04d
amv-dev <amv-dev@protonmail.com>
2021-01-13 17:05:33 +0500

    fixed errors format

--- src/core/errors.rs
+++ src/core/errors.rs
@@ -21,20 +21,22 @@ pub enum Error {
 }
 
 impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::SourceParse(value) => write!(f, "unable to parse value as Source: {:?}", value),
-            Self::ParameterParse(name, value) => write!(f, "unable to parse into {}: {:?}", name, value),
-            Self::WrongMethodParameters => write!(f, "wrong method parameters"),
-            Self::WrongConfig => write!(f, "wrong config"),
-            Self::InvalidCandles => write!(f, "invalid candles"),
-            Self::Other(reason) => write!(f, "{}", reason),
-        }
-    }
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Self::SourceParse(value) => write!(f, "Unable to parse value as Source: {:?}", value),
+			Self::ParameterParse(name, value) => {
+				write!(f, "Unable to parse into {}: {:?}", name, value)
+			}
+			Self::WrongMethodParameters => write!(f, "Wrong method parameters"),
+			Self::WrongConfig => write!(f, "Wrong config"),
+			Self::InvalidCandles => write!(f, "Invalid candles"),
+			Self::Other(reason) => write!(f, "{}", reason),
+		}
+	}
 }
 
 impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        None
-    }
+	fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+		None
+	}
 }
